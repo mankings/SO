a6ca78c3af50adeafd3c222e9d9e5b943b8cd1c3
mankings <miguelamatos02@gmail.com>
2021-12-07 22:30:46 +0000

    myActions

--- aula06/myActions.c
+++ aula06/myActions.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 /* SUGESTÂO: utilize as páginas do manual para conhecer mais sobre as funções usadas:
  man system
@@ -8,13 +10,16 @@
 */
 
 int main(int argc, char *argv[]) {
-    FILE *log = fopen("command.log", "w");
+    FILE *fp = fopen("command.log","a");
     char text[1024];
-    fprintf(log, "%d", system("date"));
+    time_t t = time(NULL);
+    struct tm tm;
 
     do {
         printf("Command: ");
         scanf("%1023[^\n]%*c", text);
+        tm = *localtime(&t);
+        t = time(NULL);
 
         /* system(const char *command) executes a command specified in command
             by calling /bin/sh -c command, and returns after the command has been
@@ -24,12 +29,15 @@ int main(int argc, char *argv[]) {
            printf("\n * Command to be executed: %s\n", text);
            printf("---------------------------------\n");
            system(text);
+           fprintf(fp,"%d:%d:%d - %s\n",tm.tm_hour,tm.tm_min,tm.tm_sec,text);
            printf("---------------------------------\n");
         }
     } while(strcmp(text, "end"));
 
+    fprintf(fp,"%d:%d:%d - end\n",tm.tm_hour,tm.tm_min,tm.tm_sec);
+    fclose(fp);
+
     printf("-----------The End---------------\n");
-    fclose(log);
     
     return EXIT_SUCCESS;
 }
