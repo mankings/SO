b5f137fc9437db7e550e434f066ca1ea2a736e1d
mankings <miguelamatos02@gmail.com>
2021-12-07 14:07:45 +0000

    Cleanup

--- aula08/fork2.c
+++ aula08/fork2.c
@@ -3,18 +3,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int ret;
 
     printf("Antes do fork: PID = %d, PPID = %d\n", getpid(), getppid());
-    if ((ret = fork()) < 0) { 
+    if ((ret = fork()) < 0) {
         perror("erro na duplicação do processo");
         return EXIT_FAILURE;
     }
-    if (ret > 0) sleep(1);
+
+    if (ret > 0) {
+        sleep(1);
+    }
     printf("Quem sou eu?\nApós o fork: PID = %d, PPID = %d, retorno do fork = %d\n",
            getpid(), getppid(), ret);
+
+    if (ret == 0) {
+        printf("FILHO\n");
+    } else {
+        printf("PAI\n");
+    }
 
     return EXIT_SUCCESS;
 }
