b0f687d892fc4a3d8e4bdaed21fe5233deb9bb80
mankings <miguelamatos02@gmail.com>
2022-01-11 16:16:37 +0000

    Sig3

--- aula08/sig3.c
+++ aula08/sig3.c
@@ -7,17 +7,17 @@
 #define NTIMES 50
 
 static void Interrupt(int);
-int count;
+unsigned int sigint_count;
 
 int main(int argc, char *argv[]) {
-    struct sigaction sigact;
+    struct sigaction sigact, oldsigact;
     unsigned int i;
 
     /* altera a rotina de atendimento ao ^C */
     sigact.sa_handler = Interrupt;
     sigemptyset(&sigact.sa_mask);
     sigact.sa_flags = 0;
-    if (sigaction(SIGINT, &sigact, NULL) < 0) {
+    if (sigaction(SIGINT, &sigact, &oldsigact) < 0) {
         perror("Rotina de atendimento não instalada\n");
         return EXIT_FAILURE;
     }
@@ -36,14 +36,16 @@ int main(int argc, char *argv[]) {
 }
 
 static void Interrupt(int signum) {
-    count++;
-    if(count == 5) {
-        printf("\nCRTL+C executado 5 vezes. Processo interrompido.\n");
-        exit(EXIT_SUCCESS);
-    }
-    
+    sigint_count++;
+
     if (signum == SIGINT) {
         printf("\nCalma, ainda não cheguei a %d!\n", NTIMES);
+        if (sigint_count == 4) {
+            struct sigaction sa;
+            sa.sa_flags = SA_RESETHAND;
+            sa.sa_handler = SIG_DFL;
+            sigaction(SIGINT, &sa, NULL);
+        }
     } else {
         printf("Foi recebido um sinal diferente de SIGINT!\n");
         exit(EXIT_FAILURE);
